e2dc7f82b52fac93de179c01ea4e7da12cdfc579
lokikongxd <lokikongxd@gmail.com>
2021-05-14 19:57:27 +0700

    update

--- client.bat
+++ client.bat
@@ -1,9 +1,6 @@
 @echo off 
-set url1=http://localhost:6336/
-set url2=https://wallet.wax.io/dashboard/
-set url3=https://all-access.wax.io/cloud-wallet/signing/
-set url4=https://all-access.wax.io/cloud-wallet/login/
+set url=http://localhost:6336/
 set /p number="Enter the number of screens you want to open: "
 for /l %%i in (1, 1, %number%) do (
-  start chrome %url1% %url2% %url3% %url4% --profile-directory="Profile %%i" --new-window
+  start chrome %url% --profile-directory="Profile %%i" --new-window 
 )
--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,20 @@
 (function () {
   // auto login
-  setTimeout(function () {
-    var login = document.getElementsByClassName("btn btn-outline-light my-2 my-sm-0")
-    if (login[0].textContent === 'Log In') {
-      login[0].click()
-    }
-  }, 3000)
+  // setTimeout(function () {
+  //   var login = document.getElementsByClassName("btn btn-outline-light my-2 my-sm-0").item(0)
+  //   if (login.textContent === 'Log In') {
+  //     login.click()
+  //   }
+  // }, 1000)
 
   // auto mine when cpu < 90%
   var WaitForReady = setInterval(function () {
-    var cpu = document.getElementsByClassName("progress-bar bg-info")
-    var mine = document.getElementsByClassName("btn btn-block btn-primary mb-2")
-    if (parseInt(cpu[0].textContent, 10) < 90) {
-      if (!mine[0].disabled && mine[0].textContent === 'Mine') {
-        mine[0].click()
+    var cpu = document.getElementsByClassName("progress-bar bg-info").item(0)
+    var mine = document.getElementsByClassName("btn btn-block btn-primary mb-2").item(0)
+    if (parseInt(cpu.textContent, 10) < 90) {
+      if (!mine.disabled && mine.textContent === 'Mine') {
+        mine.click()
       }
     }
   }, 2000)
 })()
-
