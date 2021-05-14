7875039a155898088b866092b93958fe6fdee098
lokikongxd <lokikongxd@gmail.com>
2021-05-14 22:49:05 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -8,17 +8,11 @@
   }, 1000)
 
   // mine
-  var seconds = 2000
-  const interval = setInterval(() => {
+  setInterval(() => {
     const cpu = document.getElementsByClassName("progress-bar bg-info").item(0)
     var mine = document.getElementsByClassName("btn btn-block btn-primary mb-2").item(0)
-    if (parseInt(cpu.textContent, 10) >= 100) {
-      seconds = 2000
-      clearInterval(interval)
-    }
     if (parseInt(cpu.textContent, 10) < 100 && !mine.disabled && mine.textContent === 'Mine') {
-      seconds = 36000
       mine.click()
     }
-  }, seconds)
+  }, 2000)
 })()
