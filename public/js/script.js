7a2ef7e70564f1be53a5cd7e188f4b3c34a2678a
lokikongxd <lokikongxd@gmail.com>
2021-05-15 20:20:22 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,11 @@
 (() => {
-  // login
-  setTimeout(() => {
-    const login = document.getElementsByClassName("btn btn-outline-light my-2 my-sm-0").item(0)
-    if (login.textContent === 'Log In') {
-      login.click()
-    }
-  }, 1000)
-
-  // mine
   setInterval(() => {
-    const cpu = document.getElementsByClassName("progress-bar bg-info").item(0)
-    var mine = document.getElementsByClassName("btn btn-block btn-primary mb-2").item(0)
-    if (parseInt(cpu.textContent, 10) < 100 && !mine.disabled && mine.textContent === 'Mine') {
-      mine.click()
+    const cpu = document.getElementsByClassName("progress-bar bg-info")
+    const mine = document.getElementsByClassName("btn btn-block btn-primary mb-2")
+    const autoMine = document.getElementById("autoMineCheck")
+    if (parseInt(cpu[0].textContent, 10) < 90 && !mine[0].disabled && mine[0].textContent === 'Mine' && autoMine["checked"]) {
+      mine[0].click()
     }
   }, 2000)
 })()
+
